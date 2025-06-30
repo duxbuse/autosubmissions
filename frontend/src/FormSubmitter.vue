ecef12cca9bd82d1f060a1ff918de8e1de28efd9
Sean <duxbuse@gmail.com>
2025-06-30 21:37:19 +1000

    first draft

---
+++ frontend/src/FormSubmitter.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="form-submitter">
+    <h1>Form Submission Page</h1>
+    <p>Form ID: {{ $route.params.id }}</p>
+    <p>This page will dynamically render the questions for the selected form.</p>
+  </div>
+</template>
+
---
+++ frontend/src/HomePage.vue
@@ -0,0 +1,7 @@
+<template>
+  <main>
+    <h1>Select a Form to Begin</h1>
+    <p>This page will list all available form types for submission.</p>
+  </main>
+</template>
+
